51c15992af80f794c326f3b4ee63d8fb104d2ec4
sahusayyad <sahusayyad@gmail.com>
2023-11-06 06:27:50 +0530

    Class1 added and m1 method to class JavaMain

---
+++ src/com/Class1.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Class1 {
+	public void class1Method() {
+		System.out.println("Hi I am Fine");
+	}
+
+}
--- src/com/JavaMain.java
+++ src/com/JavaMain.java
@@ -1,11 +1,17 @@
 package com;
 
 public class JavaMain {
+	
+	public void m1(){
+		System.out.println("Hi I am Method m1");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("main Method");
 
 		System.out.println("My Second statement");
+		
+		
 	}
 
 }
